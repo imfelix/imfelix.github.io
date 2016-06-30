0ca16f901043f74ad5451210f33137a04dc04175
Felix Lee <felix@netengine.com.au>
2016-06-30 10:31:48 +1000

    Add gems. Add homepage

--- Gemfile
+++ Gemfile
@@ -1,13 +1,8 @@
-# If you do not have OpenSSL installed, change
-# the following line to use 'http://'
 source 'https://rubygems.org'
+ruby '2.3.0'
 
-# For faster file watcher updates on Windows:
-gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
-
-# Windows does not come with time zone data
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
-
-# Middleman Gems
 gem 'middleman', '>= 4.0.0'
+gem 'middleman-deploy', github: 'middleman-contrib/middleman-deploy', branch: 'master'
+gem 'middleman-syntax'
 gem 'middleman-livereload'
+gem 'middleman-autoprefixer'
--- config.rb
+++ config.rb
@@ -1,44 +1,51 @@
-###
-# Page options, layouts, aliases and proxies
-###
+#################################################
+## Asset Directories
+#################################################
+config[:js_dir]     = 'assets/javascripts'
+config[:css_dir]    = 'assets/stylesheets'
+config[:images_dir] = 'assets/images'
 
-# Per-page layout changes:
-#
+#################################################
+## Templating Language
+#################################################
+set :haml, { ugly: true, format: :html5 }
+
+#################################################
+## Developement Configuration
+#################################################
+configure :development do
+  activate :autoprefixer
+  activate  :livereload
+  activate  :syntax
+end
+
+#################################################
+## Build Configuration
+#################################################
+configure :build do
+  activate  :minify_css
+  activate  :minify_javascript
+  activate  :relative_assets
+  set       :relative_links, true
+end
+
+#################################################
+## Deploy Configuration
+#################################################
+# To add CNAME, simply add it under the source directory
+activate :deploy do |deploy|
+  deploy.deploy_method  = :git
+  deploy.branch         = 'gh-pages'
+  deploy.build_before   = true
+  deploy.commit_message = 'Deploying to gh-pages'
+end
+
+#################################################
+## Page Options and Layouts
+#################################################
 # With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
+# page '/*.xml', layout: false
+# page '/*.json', layout: false
+# page '/*.txt', layout: false
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-end
