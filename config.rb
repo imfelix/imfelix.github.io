37a1a493c13d2c2a2e77d5e89d9f75369fb41808
Felix Lee <felix@netengine.com.au>
2017-04-06 22:01:07 +1000

    Added sitemap config. Updated twitter summary card

--- config.rb
+++ config.rb
@@ -83,7 +83,7 @@ page "/articles.html",      layout: "layout-articles-list"
 page "/projects.html",      layout: "layout-projects-list"
 page "/get-in-touch.html",  layout: "layout-sans-header"
 page "/thanks.html",        layout: "layout-sans-header"
-page '/sitemap.xml',        layout: false
+page "/sitemap.xml",        layout: false
 
 # With no layout
 # page '/*.xml', layout: false
