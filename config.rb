b8b895cca8ce8565ce9c101c3da29422a80a1c44
imfelix <felixlee.piat@gmail.com>
2018-05-15 14:21:31 +1000

    Added Haml 5 Warnings Workaround

--- config.rb
+++ config.rb
@@ -13,7 +13,6 @@ config[:images_dir] = 'assets/images'
 #################################################
 ## Templating Language
 #################################################
-set :haml, { ugly: true, format: :html5 }
 set :markdown_engine, :redcarpet
 set :markdown,
     :fenced_code_blocks => true,
@@ -24,6 +23,26 @@ set :markdown,
     :strikethrough => true,
     :quote => true,
     :footnotes => true
+    
+## Start - Haml 5 Warnings Workaround
+::Haml::TempleEngine.define_options context: nil
+::HamlPatch ||= Module.new {
+  # https://github.com/middleman/middleman/commit/02f95aa43980aac82455228aede681daf4fbb65c
+  def evaluate(scope, locals, &block)
+    options = {}.merge!(@options).merge!(filename: eval_file, line: line, context: @context || scope)
+    if options.include?(:outvar)
+      options[:buffer] = options.delete(:outvar)
+      options[:save_buffer] = true
+    end
+    @engine = ::Haml::Engine.new(data, options)
+    output = @engine.render(scope, locals, &block)
+    output
+  end
+}
+p patching_haml: ::HamlPatch
+
+::Middleman::Renderers::HamlTemplate.prepend ::HamlPatch
+## End - Haml 5 Warnings Workaround
 
 #################################################
 ## Blog Configuration
